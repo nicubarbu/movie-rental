e835d6609d4d1bd76868960eaa8676111db9ac73
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-12-15 20:51:33 +0200

    Create entity.py

---
+++ domain/entity.py
@@ -0,0 +1,12 @@
+class Entity:
+    def __init__(self, id_entity):
+        self.__id_entity = id_entity
+        
+    # @property 
+    def get_id_entity(self):
+        return self.__id_entity
+    
+    # @get_id_entity.setter
+    def set_id_entity(self, id_entity):
+        self.__id_entity = id_entity
+        
