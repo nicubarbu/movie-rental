71c6082987f5b9adab8d7b9cf9c002b92a648293
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-12-19 16:06:48 +0200

    Split file repository into client and movie file repositories

---
+++ repository/client_file_repository.py
@@ -0,0 +1,42 @@
+from domain.client import Client
+from domain.movie import Movie
+from repository.repository import Repository
+
+
+class FileClientRepository(Repository):
+    def __init__(self, file_name):
+        super().__init__()
+        self.__file_name = file_name
+        self.__read_file()
+        
+    def add(self, client):
+        super().add(client)
+        self.__write_file()
+        
+    def modify(self, client):
+        super().modify(client)
+        self.__write_file()
+        
+    def remove(self, id_client):
+        super().remove(id_client)
+        self.__write_file()
+        
+    def __read_file(self):
+        try:
+            with open(self.__file_name, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    id_client = line.split()[0]
+                    name = line.split()[1]
+                    pin = line.split()[2]
+                    client = Client(int(id_client), name, pin)
+                    self._entities[client.get_pin] = client
+        except IOError:
+            raise IOError("File could not be opened!")
+        
+    def __write_file(self):
+        with open(self.__file_name, 'w') as f:
+            for client in self.get_all():
+                f.write(f'{client.get_id_entity} {client.get_name} {client.get_pin}')
+                
+    
