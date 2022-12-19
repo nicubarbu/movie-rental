d0c12753b30875b74a5e947fac058c88f45578e9
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-12-20 01:20:37 +0200

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,8 +5,11 @@ from tests.test_domain.test_movie import TestMovie
 from tests.test_domain.test_entity import TestEntity
 from tests.test_domain.test_client_movie import TestClientMovie
 from tests.test_repository.test_repository import TestRepository
+from tests.test_repository.test_client_file_repository import TestFileClientRepository
+from tests.test_repository.test_movie_file_repository import TestFileMovieRepository
 from tests.test_service.test_client_service import TestClientService
 from tests.test_service.test_movie_service import TestMovieService
+from tests.test_service.test_client_movie_service import TestClientMovieService
 
 
 def run_tests():
@@ -17,9 +20,12 @@ def run_tests():
             loader.loadTestsFromTestCase(TestMovie),
             loader.loadTestsFromTestCase(TestEntity),
             loader.loadTestsFromTestCase(TestClientMovie),
-            # loader.loadTestsFromTestCase(TestRepository),
+            loader.loadTestsFromTestCase(TestRepository),
+            loader.loadTestsFromTestCase(TestFileClientRepository),
+            loader.loadTestsFromTestCase(TestFileMovieRepository),
             loader.loadTestsFromTestCase(TestClientService),
-            loader.loadTestsFromTestCase(TestMovieService)
+            loader.loadTestsFromTestCase(TestMovieService),
+            loader.loadTestsFromTestCase(TestClientMovieService)
         ]
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(unittest.TestSuite(all_suites))
