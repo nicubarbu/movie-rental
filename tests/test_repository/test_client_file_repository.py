4b7908c0ebc5240871435952817b2137fcf288d7
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-12-20 00:37:57 +0200

    Added tests

---
+++ tests/test_repository/test_client_file_repository.py
@@ -0,0 +1,52 @@
+from domain.client import Client
+from repository.client_file_repository import FileClientRepository
+import pathlib
+import os
+import unittest
+
+TESTDATA_FILENAME: str = os.path.join(pathlib.Path(__file__).parent.absolute(), 'test.txt')
+
+
+class TestFileClientRepository(unittest.TestCase):
+    def setUp(self):
+        self.testfile = open(TESTDATA_FILENAME)
+        self.testdata = self.testfile.read()
+
+    def tearDown(self):
+        self.testfile.close()
+        with open(TESTDATA_FILENAME, mode='w'):
+            pass
+
+    def test_add(self):
+        repo = FileClientRepository(TESTDATA_FILENAME)
+        repo.add(Client(1, 'name', 'pin'))
+        self.assertEqual(len(repo.get_all()), 1)
+        
+    def test_modify(self):
+        repo = FileClientRepository(TESTDATA_FILENAME)
+        repo.add(Client(1, 'name', 'pin'))
+        repo.modify(Client(1, 'name2', 'pin2'))
+        with self.assertRaises(KeyError):
+            repo.modify(Client(2, 'name2', 'pin2'))
+        self.assertEqual(repo.get_all()[0].get_name, 'name2')
+        self.assertEqual(repo.get_all()[0].get_pin, 'pin2')
+        
+    def test_remove(self):
+        repo = FileClientRepository(TESTDATA_FILENAME)
+        repo.add(Client(1, 'name', 'pin'))
+        repo.remove(1)
+        self.assertEqual(len(repo.get_all()), 0)
+        
+    def test_read_from_file(self):
+        TESTDATA_FILENAME: str = os.path.join(pathlib.Path(__file__).parent.absolute(), 'clients.txt')
+        self.testfile = open(TESTDATA_FILENAME)
+        self.testdata = self.testfile.read()
+        repo = FileClientRepository(TESTDATA_FILENAME)
+        self.assertEqual(repo.get_all()[0].get_name, 'nicu')
+
+    def test_not_exists(self):
+        try:
+            repo = FileClientRepository("")
+            # assert False, "repo should throw exception if file doesn't exist"
+        except IOError:
+            pass
---
+++ tests/test_repository/test_movie_file_repository.py
@@ -0,0 +1,54 @@
+from domain.movie import Movie
+from repository.movie_file_repository import FileMovieRepository
+import pathlib
+import os
+import unittest
+
+TESTDATA_FILENAME: str = os.path.join(pathlib.Path(__file__).parent.absolute(), 'test.txt')
+
+
+class TestFileMovieRepository(unittest.TestCase):
+    def setUp(self):
+        self.testfile = open(TESTDATA_FILENAME)
+        self.testdata = self.testfile.read()
+        
+    def tearDown(self):
+        self.testfile.close()
+        with open(TESTDATA_FILENAME, mode='w'):
+            pass
+        
+    def test_add(self):
+        repo = FileMovieRepository(TESTDATA_FILENAME)
+        repo.add(Movie(1, 'Thor', 'description', 'genre'))
+        self.assertEqual(len(repo.get_all()), 1)
+        
+    def test_modify(self):
+        repo = FileMovieRepository(TESTDATA_FILENAME)
+        repo.add(Movie(1, 'Thor', 'description', 'genre'))
+        repo.modify(Movie(1, 'Thor2', 'description2', 'genre2'))
+        with self.assertRaises(KeyError):
+            repo.modify(Movie(2, 'Thor2', 'description2', 'genre2'))
+        self.assertEqual(repo.get_all()[0].get_name, 'Thor2')
+        self.assertEqual(repo.get_all()[0].get_description, 'description2')
+        self.assertEqual(repo.get_all()[0].get_genre, 'genre2')
+        
+    def test_remove(self):
+        repo = FileMovieRepository(TESTDATA_FILENAME)
+        repo.add(Movie(1, 'Thor', 'description', 'genre'))
+        repo.remove(1)
+        self.assertEqual(len(repo.get_all()), 0)
+        
+    def test_read_from_file(self):
+        TESTDATA_FILENAME: str = os.path.join(pathlib.Path(__file__).parent.absolute(), 'movies.txt')
+        self.testfile = open(TESTDATA_FILENAME)
+        self.testdata = self.testfile.read()
+        repo = FileMovieRepository(TESTDATA_FILENAME)
+        self.assertEqual(repo.get_all()[1].get_name, 'Thor')
+        
+    def test_not_exists(self):
+        try:
+            repo = FileMovieRepository("")
+            # assert False, "repo should throw exception if file doesn't exist"
+        except IOError:
+            pass
+    
