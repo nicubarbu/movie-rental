41f8e7c9a16f5ebfbddf5704433ac7e662aa667a
Nicu Barbu <barbu_nicusor6@yahoo.com>
2023-01-08 12:01:04 +0200

    Create test_client_movie_service.py

---
+++ tests/test_service/test_client_movie_service.py
@@ -0,0 +1,9 @@
+from repository.repository import Repository
+from services.client_movie_service import RentService
+from services.client_service import ClientService
+from services.movie_service import MovieService
+import unittest
+
+
+# class TestClientMovieService(unittest.TestCase):
+    
